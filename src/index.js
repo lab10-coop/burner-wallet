6099d52d571ba5a838f751370c022bdccac6ebd3
Thomas Haller <Thomas.Haller@lab10.coop>
2019-04-04 12:07:02 +0200

    service worker for progressive webapp

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
+serviceWorker.register();
 
 ReactDOM.render(<App />, document.getElementById('root'));
