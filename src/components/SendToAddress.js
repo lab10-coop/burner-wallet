a54e75ed9e9064ff8068b70d109b6a4e66268003
Thomas Haller <Thomas.Haller@lab10.coop>
2019-07-03 13:33:37 +0200

    fixed "send all" button after increasing the transaction fee to 100 gwei.

--- src/components/SendToAddress.js
+++ src/components/SendToAddress.js
@@ -158,8 +158,7 @@ export default class SendToAddress extends React.Component {
   }
 
   calcCurrentGasCosts(){
-    //todo: currently hardcoded 1 gwei.
-    return this.currentTxGasLimit / 100000000000.0;
+    return this.currentTxGasLimit / 10000000.0;
   }
 
   send = async () => {
